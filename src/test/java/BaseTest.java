da70d42248f6013d078c2284887880fca62f7f8b
apollo <apoll0time739@gmail.com>
2024-02-05 12:27:17 +0900

    test1

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,2 +1,25 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class BaseTest {
+
+    @Test
+    public void openBrowser() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://www.jango.com/");
+        WebElement loginButton = driver.findElement(By.xpath("//a[text()='Log In']"));
+        Assert.assertTrue(loginButton.isDisplayed());
+        driver.close();
+    }
 }
